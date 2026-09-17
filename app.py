0463e367e13d30a047adada8abdbd111c5352edf
Muthupriya2025 <muthupriyapriya78@gmail.com>
2026-09-17 16:54:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def chat():
 
     try:
         response = client.models.generate_content(
-            model="gemini-3.8-flash",
+            model="gemini-3.7-flash",
             contents=conversation
         )
 
